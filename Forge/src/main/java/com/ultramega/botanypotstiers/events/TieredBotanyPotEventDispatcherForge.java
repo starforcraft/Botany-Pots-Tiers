c8b7755a5bd6c821055532f010bb2d92f6249ad3
Starforcraft <starforcraft@yahoo.com>
2022-10-16 14:09:29 +0200

    Fixed Seed & Soil disappearing after unloading the chunk the pot is in

--- Forge/src/main/java/com/ultramega/botanypotstiers/events/TieredBotanyPotEventDispatcherForge.java
+++ Forge/src/main/java/com/ultramega/botanypotstiers/events/TieredBotanyPotEventDispatcherForge.java
@@ -19,7 +19,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
-public class TieredBotanyPotEventDispatcherForge implements BotanyPotEventDispatcher {
+public class TieredBotanyPotEventDispatcherForge implements TieredBotanyPotEventDispatcher {
     @Nullable
     @Override
     public Soil postSoilLookup(Level level, BlockPos pos, TieredBlockEntityBotanyPot pot, ItemStack stack, @Nullable Soil found) {
