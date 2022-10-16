62f04cc49ca0c1a5d246db893391074416e2519e
TheFakeJaneDoe <evanblankenship1@gmail.com>
2022-10-16 15:17:41 -0400

    Test

---
+++ Common/src/main/java/com/ultramega/botanypotstiers/TieredBotanyPotsUtils.java
@@ -0,0 +1,16 @@
+package com.ultramega.botanypotstiers;
+
+public class TieredBotanyPotsUtils {
+
+	public static int ELITE_MULTIPLIER;
+	public static int ELITE_SPEED;
+
+	public static int ULTRA_MULTIPLIER;
+	public static int ULTRA_SPEED;
+	
+	public static int CREATIVE_MULTIPLIER;
+	public static int CREATIVE_SPEED;
+	
+
+	
+}
--- Fabric/src/main/java/com/ultramega/botanypotstiers/TieredBotanyPotsFabric.java
+++ Fabric/src/main/java/com/ultramega/botanypotstiers/TieredBotanyPotsFabric.java
@@ -1,10 +1,13 @@
 package com.ultramega.botanypotstiers;
 
+import com.ultramega.botanypotstiers.config.TieredBotanyPotsFabricConfig;
+
 import net.fabricmc.api.ModInitializer;
 
 public class TieredBotanyPotsFabric implements ModInitializer {
     @Override
     public void onInitialize() {
+    	TieredBotanyPotsFabricConfig.setupValues();
         new TieredBotanyPotsCommon();
     }
 }
