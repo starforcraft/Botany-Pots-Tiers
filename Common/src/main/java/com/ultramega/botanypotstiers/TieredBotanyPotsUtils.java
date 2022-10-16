644e8a9ff97427dad6e29f98561c3d331e1266fb
BuildTools <unconfigured@null.spigotmc.org>
2022-10-16 12:28:32 -0400

    Forge Configuration Added

---
+++ Common/src/main/java/com/ultramega/botanypotstiers/TieredBotanyPotsUtils.java
@@ -0,0 +1,16 @@
+package com.ultramega.botanypotstiers;
+
+public class TieredBotanyPotsUtils {
+
+	public static int ELITE_MULTIPLIER = 1;
+	public static int ELITE_SPEED = 1;
+
+	public static int ULTRA_MULTIPLIER = 1;
+	public static int ULTRA_SPEED = 1;
+	
+	public static int CREATIVE_MULTIPLIER = 1;
+	public static int CREATIVE_SPEED = 1;
+	
+
+	
+}
